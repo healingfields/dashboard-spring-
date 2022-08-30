139655145408bb2e0313131f8168631b97154912
healingfields <dihratene@gmail.com>
2022-08-30 20:40:18 +0100

    added lombok dependency and added version to maven plugin

---
+++ src/test/java/ma/showmaker/dashboard/DashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.showmaker.dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
